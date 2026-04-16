2064e0fc3734d777e136a411dea4909c809ff102
Neupane07 <70212703+Neupane07@users.noreply.github.com>
2026-04-16 16:05:36 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let solution = [...nums];
+        for (let i = 0; i < nums.length; i++) {
+            let product = 1;
+            for (let j = 0; j < nums.length; j++) {
+                if (i === j) continue;
+
+                product *= nums[j];
+            }
+            solution[i] = product;
+        }   
+        return solution;     
+    }
+}
