a94b4db43fadc13c4cc057668101657a39a8cc74
Neupane07 <70212703+Neupane07@users.noreply.github.com>
2026-05-11 21:43:55 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        const n = heights.length;
+
+        let maxArea = 0;
+        let left = 0;
+        let right = n - 1;
+        while(left < right) {
+            const smallerHeight = Math.min(heights[left], heights[right]);
+            const currentArea = smallerHeight * (right-left)
+
+            maxArea = Math.max(maxArea, currentArea);
+            if(heights[left] > heights[right]) {
+                right--;
+            } else {
+                left++;
+            }
+            
+        }
+
+        return  maxArea;
+    }
+}
