fcc367553b5840ea9c1ff183d8f72c9038e7b787
Neupane07 <70212703+Neupane07@users.noreply.github.com>
2026-09-08 10:15:09 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if (s.length % 2 !== 0) return false;
+
+        const stack = [];
+
+        for (let i = 0; i < s.length; i++) {
+            const char = s[i];
+
+            if (char === '(') {
+                stack.push(')');
+            } else if (char === '{') {
+                stack.push('}');
+            } else if (char === '[') {
+                stack.push(']');
+            } else if (stack.pop() !== char) {
+                return false;
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
