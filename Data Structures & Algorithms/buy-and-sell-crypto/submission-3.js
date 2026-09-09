2cae7ce8445ba8f26042486d9a7ba3015a898ab0
Neupane07 <70212703+Neupane07@users.noreply.github.com>
2026-09-09 12:55:49 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        // 10 1 5 6 7 1
+
+        let maxProfit = 0;
+
+        for (let i = 0; i < prices.length; i++) {
+            for (let j = i+1; j < prices.length; j++) {
+                maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
+            }
+        }
+
+        return maxProfit;
+    }
+}
