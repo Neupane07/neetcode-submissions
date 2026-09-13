a9b3a1c5591b1580d24288c78ba87a2a5dea0452
Neupane07 <70212703+Neupane07@users.noreply.github.com>
2026-09-13 13:29:23 +0530

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.js
@@ -0,0 +1,40 @@
+class MinStack {
+    constructor() {
+        // [val, currentMin][]
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        if (this.stack.length === 0) {
+            this.stack.push([val, val]);
+        } else {
+            const currentMin = this.stack.at(-1)[1];
+            this.stack.push([val, Math.min(currentMin, val)]);
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack.at(-1)[0];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.stack.at(-1)[1];
+    }
+}
